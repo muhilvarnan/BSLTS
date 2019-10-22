07040c7d3e0c0b87a6f60abf027a37494eccda40
muhilvarnan <muhilvarnan.v@gmail.com>
2019-10-22 21:22:28 +0530

    Team and participants

--- game_manager/admin.py
+++ game_manager/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Zone, District
+from .models import Zone, District, Participant, Team
 
 
 class ZoneAdmin(admin.ModelAdmin):
@@ -11,5 +11,21 @@ class DistrictAdmin(admin.ModelAdmin):
     list_display = ('name', 'zone', 'contact_name', 'contact_phone_number', 'contact_email')
 
 
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'date_of_birth', 'district')
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'total_participants')
+
+    def total_participants(self, obj):
+        return obj.participants.count()
+
+    total_participants.empty_value_display = 0
+
+
 admin.site.register(Zone, ZoneAdmin)
 admin.site.register(District, DistrictAdmin)
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Team, TeamAdmin)
+
--- game_manager/models.py
+++ game_manager/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 import uuid
+import random
+import string
+
+
+def random_string(string_length=5):
+    """Generate a random string of fixed length """
+    letters = string.ascii_uppercase + string.digits
+    return ''.join(random.choice(letters) for i in range(string_length))
 
 
 class Zone(models.Model):
@@ -20,3 +28,25 @@ class District(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Participant(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    code = models.CharField(max_length=255, default=random_string)
+    name = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.code + " - " + self.name
+
+
+class Team(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    code = models.CharField(max_length=255, default=random_string)
+    name = models.CharField(max_length=255)
+    participants = models.ManyToManyField(Participant)
+
+    def __str__(self):
+        return self.code + " - " + self.name
+
